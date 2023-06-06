f35930f8c88dc52d04bf7df634490c8754ef28f0
Shepon Biswas <sheponbiswas01@gmail.com>
2023-06-07 04:06:53 +0600

    Password validation applied & show a sweet alert after successfull register or login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,10 +3,10 @@ import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import SocialLogin from "../shared/SocialLogin/SocialLogin";
 import { AuthContext } from "../../providers/AuthProvider";
+import Swal from "sweetalert2";
 
 const Login = () => {
-
-    const {loginUser, googleSignIn} = useContext(AuthContext)
+  const { loginUser, googleSignIn } = useContext(AuthContext);
   const {
     register,
     handleSubmit,
@@ -18,30 +18,38 @@ const Login = () => {
     const password = data.password;
 
     loginUser(email, password)
-    .then((result)=>{
+      .then((result) => {
         const loggedUser = result.user;
-        console.log(loggedUser)
-    })
-    .then((error) => console.log(error))
+        // console.log(loggedUser)
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "User Login Successfully",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      })
+      .then((error) => console.log(error));
   };
 
-//   Google Sign in Method
-const handleGoogleSignIn = ()=>
-googleSignIn()
-.then(result =>{
-    const user = result.user;
-    console.log(user)
-})
-.catch(error =>{
-    console.error(error)
-})
-
+  //   Google Sign in Method
+  const handleGoogleSignIn = () =>
+    googleSignIn()
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
 
   return (
     <>
       <div className="w-11/12 md:w-10/12 mx-auto">
         <div className="my-5">
-            <h3 className="text-center text-3xl font-bold text-[#066466]">Please Login</h3>
+          <h3 className="text-center text-3xl font-bold text-[#066466]">
+            Please Login
+          </h3>
         </div>
         <div className="bg-gray-100 grid md:grid-cols-2 gap-8 p-8 rounded-md">
           <div>
@@ -64,11 +72,15 @@ googleSignIn()
                   placeholder="example@gmail.com"
                   className="input input-bordered w-full border-[#066466] "
                 />
-                {errors.email && <span className="text-red-500">This field is required</span>}
+                {errors.email && (
+                  <span className="text-red-500">This field is required</span>
+                )}
               </div>
               <div className="form-control w-full">
                 <label className="label">
-                  <span className="label-text font-semibold">Enter Password</span>
+                  <span className="label-text font-semibold">
+                    Enter Password
+                  </span>
                 </label>
                 <input
                   {...register("password", { required: true })}
@@ -76,13 +88,27 @@ googleSignIn()
                   placeholder="type password"
                   className="input input-bordered w-full border-[#066466] "
                 />
-                {errors.password && <span className="text-red-500">This field is required</span>}
+                {errors.password && (
+                  <span className="text-red-500">This field is required</span>
+                )}
               </div>
 
-              <input className="btn bg-[#066466] w-full my-3 text-white  hover:bg-white hover:text-[#066466] hover: border-2 border-[#066466]" type="submit" value={"Login"} />
+              <input
+                className="btn bg-[#066466] w-full my-3 text-white  hover:bg-white hover:text-[#066466] hover: border-2 border-[#066466]"
+                type="submit"
+                value={"Login"}
+              />
             </form>
             <div>
-                <span>Don't Have Account? <Link className="underline text-[#066466] font-semibold" to={"/registration"}>Register</Link></span>
+              <span>
+                Don't Have Account?{" "}
+                <Link
+                  className="underline text-[#066466] font-semibold"
+                  to={"/registration"}
+                >
+                  Register
+                </Link>
+              </span>
             </div>
             <SocialLogin handleGoogleSignIn={handleGoogleSignIn}></SocialLogin>
           </div>
--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -1,10 +1,12 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../providers/AuthProvider";
+import Swal from "sweetalert2";
 
 const Registration = () => {
-    const {createNewUser, updateUserInfo} = useContext(AuthContext);
+  const { createNewUser, updateUserInfo } = useContext(AuthContext);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const {
     register,
@@ -17,26 +19,59 @@ const Registration = () => {
     const password = data.password;
     const photo = data.photo;
     const name = data.name;
+    const confirmPassword = data.confirmPassword;
+
+    // Validate Password field[one-specail char, one digit/number, one uppercase]
+    if (!/(?=.*[A-Z])/.test(password)) {
+      setErrorMessage("Password: Please add at least one uppercase letter");
+      return;
+    } else if (!/(?=.*[0-9])/.test(password)) {
+      setErrorMessage("Password: Please add at least one number");
+      return;
+    } else if (!/(?=.*?[#?!@$%^&*-])/.test(password)) {
+      setErrorMessage("Password: Must Have One Special Character [#?!@$%^&*-]");
+      return;
+    } else if (password.length < 6) {
+      setErrorMessage("Password must be 6 characters or more");
+      return;
+    }else if(password !== confirmPassword){
+        setErrorMessage("Password: Password doesn't matched!");
+        return;
+    }
+
+
     createNewUser(email, password)
-    .then((res) =>{
+      .then((res) => {
+        setErrorMessage("");
         const newUser = res.user;
         console.log(newUser);
         // Update new user displayName & Photo URL
         updateUserInfo(newUser, name, photo)
-        .then(()=>{})
-        .catch((error) =>{
-            setErrorMessage(error.message)
-        })
-    })
-    .catch((error) => console.log(error))
-    
-    console.log(data)};
+          .then(() => {
+            Swal.fire({
+              position: "top-end",
+              icon: "success",
+              title: "User Created Successfully",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+          })
+          .catch((error) => {
+            setErrorMessage(error.message);
+          });
+      })
+      .catch((error) => console.log(error));
+
+    console.log(data);
+  };
 
   return (
     <>
       <div className="w-11/12 md:w-10/12 mx-auto">
         <div className="my-5">
-            <h3 className="text-center text-3xl font-bold text-[#066466]">Please Register</h3>
+          <h3 className="text-center text-3xl font-bold text-[#066466]">
+            Please Register
+          </h3>
         </div>
         <div className="bg-gray-100 grid md:grid-cols-2 gap-8 p-8 rounded-md">
           <div>
@@ -70,11 +105,15 @@ const Registration = () => {
                   placeholder="example@gmail.com"
                   className="input input-bordered w-full border-[#066466] "
                 />
-                {errors.email && <span className="text-red-500">This field is required</span>}
+                {errors.email && (
+                  <span className="text-red-500">This field is required</span>
+                )}
               </div>
               <div className="form-control w-full">
                 <label className="label">
-                  <span className="label-text font-semibold">Enter Password</span>
+                  <span className="label-text font-semibold">
+                    Enter Password
+                  </span>
                 </label>
                 <input
                   {...register("password", { required: true })}
@@ -82,11 +121,16 @@ const Registration = () => {
                   placeholder="type password"
                   className="input input-bordered w-full border-[#066466] "
                 />
-                {errors.password && <span className="text-red-500">This field is required</span>}
+                {errors.password && (
+                  <span className="text-red-500">This field is required</span>
+                )}
+                <p className="text-red-600">{errorMessage}</p>
               </div>
               <div className="form-control w-full">
                 <label className="label">
-                  <span className="label-text font-semibold">Confirm Password</span>
+                  <span className="label-text font-semibold">
+                    Confirm Password
+                  </span>
                 </label>
                 <input
                   {...register("confirmPassword", { required: true })}
@@ -94,32 +138,42 @@ const Registration = () => {
                   placeholder="re-type password"
                   className="input input-bordered w-full border-[#066466] "
                 />
-                {errors.confirmPassword && <span className="text-red-500">This field is required</span>}
+                {errors.confirmPassword && (
+                  <span className="text-red-500">This field is required</span>
+                )}
+                <p className="text-red-600">{errorMessage}</p>
               </div>
               <div className="form-control w-full">
                 <label className="label">
                   <span className="label-text font-semibold">Photo URL</span>
                 </label>
                 <input
-                  {...register("photo")}
+                  {...register("photo", { required: true })}
                   type="link"
                   placeholder="paste photo url"
                   className="input input-bordered w-full border-[#066466] "
                 />
               </div>
               <div className="form-control w-full">
-              <label className="label">
-                  <span className="label-text font-semibold">Select Gender</span>
+                <label className="label">
+                  <span className="label-text font-semibold">
+                    Select Gender
+                  </span>
                 </label>
-              <select {...register("gender")} className="select select-bordered border-[#066466] w-full">
-                <option value="female">Female</option>
-                <option value="male">Male</option>
-                <option value="other">Other</option>
-              </select>
+                <select
+                  {...register("gender")}
+                  className="select select-bordered border-[#066466] w-full"
+                >
+                  <option value="female">Female</option>
+                  <option value="male">Male</option>
+                  <option value="other">Other</option>
+                </select>
               </div>
               <div className="form-control w-full">
                 <label className="label">
-                  <span className="label-text font-semibold">Your Phone Number</span>
+                  <span className="label-text font-semibold">
+                    Your Phone Number
+                  </span>
                 </label>
                 <input
                   {...register("phone")}
@@ -139,10 +193,22 @@ const Registration = () => {
                   className="input input-bordered w-full border-[#066466] "
                 />
               </div>
-              <input className="btn bg-[#066466] w-full my-3 text-white  hover:bg-white hover:text-[#066466] hover: border-2 border-[#066466]" type="submit" value={"Registration"} />
+              <input
+                className="btn bg-[#066466] w-full my-3 text-white  hover:bg-white hover:text-[#066466] hover: border-2 border-[#066466]"
+                type="submit"
+                value={"Registration"}
+              />
             </form>
             <div>
-                <span>Already Have an Account? <Link className="underline text-[#066466] font-semibold" to={"/login"}>Login</Link></span>
+              <span>
+                Already Have an Account?{" "}
+                <Link
+                  className="underline text-[#066466] font-semibold"
+                  to={"/login"}
+                >
+                  Login
+                </Link>
+              </span>
             </div>
           </div>
         </div>
