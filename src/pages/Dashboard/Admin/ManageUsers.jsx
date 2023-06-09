b6f7698b607435462d38ecd160cbfdc125951f7e
Shepon Biswas <sheponbiswas01@gmail.com>
2023-06-10 00:11:12 +0600

    Implemented Make Admin Role Functionality on Admin, Manage Users Route

--- src/pages/Dashboard/Admin/ManageUsers.jsx
+++ src/pages/Dashboard/Admin/ManageUsers.jsx
@@ -1,14 +1,39 @@
-import React from 'react';
-import { FaUserTie, FaUsersCog } from 'react-icons/fa';
-import { useQuery } from 'react-query';
+import React from "react";
+import { FaUserTie, FaUsersCog } from "react-icons/fa";
+import { useQuery } from "react-query";
+import Swal from "sweetalert2";
 
 const ManageUsers = () => {
-    const { data: users = [], refetch } = useQuery(["users"], async () => {
-        const res = await fetch("http://localhost:5000/users");
-        return res.json();
+  const { data: users = [], refetch } = useQuery(["users"], async () => {
+    const res = await fetch("http://localhost:5000/users");
+    return res.json();
+  });
+
+  //   Handle Make Admin Function
+  const handleMakeAdmin = (user) => {
+    console.log(user);
+    fetch(`http://localhost:5000/users/admin/${user._id}`, {
+      method: "PATCH",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount) {
+          refetch();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${user.name} is an Admin Now!!!`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
       });
-    return (
-        <>
+  };
+
+
+  return (
+    <>
       <h2>All Users</h2>
       <span>{users.length}</span>
       {/* User Table */}
@@ -26,36 +51,48 @@ const ManageUsers = () => {
             </tr>
           </thead>
           <tbody>
-            {
-            users.map((user, index) => <tr key={user._id}>
-                <th>{index + 1 }</th>
+            {users.map((user, index) => (
+              <tr key={user._id}>
+                <th>{index + 1}</th>
                 <td>{user.name}</td>
-                
+
                 <td>
                   <div className="avatar">
                     <div className="mask mask-squircle w-12 h-12">
-                      <img
-                        src={user.image}
-                        alt="User Image"
-                      />
+                      <img src={user.image} alt="User Image" />
                     </div>
                   </div>
                 </td>
                 <td>{user.email}</td>
-                <td>{user.role === "admin" ? "Admin": user.role === "instructor" ? "Instructor" : " Student"}</td>
                 <td>
-                  <button title="Make Admin" className="btn btn-sm text-white bg-[#066466] hover:text-orange-500 me-2 "><FaUsersCog></FaUsersCog> </button>
-                  <button title="Make Instructor" className="btn btn-sm bg-orange-400"><FaUserTie></FaUserTie> </button>
+                  {user.role === "admin"
+                    ? "Admin"
+                    : user.role === "instructor"
+                    ? "Instructor"
+                    : " Student"}
                 </td>
-                
-              </tr> )
-            }
-            
+                <td>
+                  <button
+                  onClick={()=>handleMakeAdmin(user)}
+                    title="Make Admin"
+                    className="btn btn-sm text-white bg-[#066466] hover:text-orange-500 me-2 "
+                  >
+                    <FaUsersCog></FaUsersCog>
+                  </button>
+                  <button
+                    title="Make Instructor"
+                    className="btn btn-sm bg-orange-400"
+                  >
+                    <FaUserTie></FaUserTie>
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
     </>
-    );
+  );
 };
 
 export default ManageUsers;
